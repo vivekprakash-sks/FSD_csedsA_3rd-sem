18b99dfab840eee8d5eba06a31df8b1a4852ed44
Vivek Prakash <vivekprakash02052004@gmail.com>
2026-09-01 12:02:43 +0530

    Event Emitter in node js

--- eventEmitter.js
+++ eventEmitter.js
@@ -1,10 +1,14 @@
 const EventEmitter = require('events');
 const myEmitter = new EventEmitter();
+//.on() id the listener for the events
+//.emit() id used to trigger  the events
 myEmitter.on('greet', (name) => {
     console.log('hello, $ { name }!Welcome to Node js')
 });
 myEmitter.on('exit', () => {
     console.log("Application Closed. ");
 });
+//Emit the "Greet"  event
 myEmitter.emit('greet', '2nd Year');
+//Emit the "Exit"  event
 myEmitter.emit('exit');
